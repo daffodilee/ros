e72e67fed514134032ffa9a78f4821ec1d4aeef0
daffodilee <147146586+daffodilee@users.noreply.github.com>
2024-12-05 17:37:27 +0500

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/huseyin/Desktop/robotics_HW/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/huseyin/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/huseyin/catkin_ws/devel;/opt/ros/noetic' != '' else []
